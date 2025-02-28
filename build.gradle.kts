ebe2e9b6bcd98f0e0b08ef72e3494a27b770cd32
Baconing <iam@baconi.ng>
2025-02-28 11:13:44 -0500

    revert: "Bump org.junit:junit-bom from 5.11.4 to 5.12.0"
    This reverts commit 8206cc2733125b3ff0af0181791b06cbf6615afc.
    
    Caused junit versions to be out of sync, making test builds fail.

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,7 +111,7 @@ dependencies {
     nativeRelease(wpi.java.vendor.jniRelease(NativePlatforms.desktop))
     simulationRelease(wpi.sim.enableRelease())
 
-    testImplementation(platform("org.junit:junit-bom:5.12.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
