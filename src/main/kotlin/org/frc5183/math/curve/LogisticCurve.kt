e30495c1643d2dcb206054a67378d015a2ec049b
Baconing <iam@baconi.ng>
2025-03-18 09:50:47 -0400

    fix(math/curve): fill logistic curve hole at x=0

--- src/main/kotlin/org/frc5183/math/curve/LogisticCurve.kt
+++ src/main/kotlin/org/frc5183/math/curve/LogisticCurve.kt
@@ -9,7 +9,7 @@ import kotlin.math.sign
  * Represents a logistic [Curve] with some modifications:
  * - Symmetric about the y-axis, allowing for negative inputs.
  * - Uses a base of 1 + e. (where e is Euler's number). Applies an asymptote to the curve at y= -1 or 1.
- * - Fills the holes at x = -1 and 1 with -1 and 1 respectively.
+ * - Fills the holes at x = -1, 0, and 1 with -1, 0, and 1 respectively.
  *
  * The equation for this curve is y = 1 / (1 + e^(-[a] * abs(x) - [b]))
  */
@@ -24,12 +24,11 @@ class LogisticCurve(
      */
     val b: Double,
 ) : Curve {
-    override operator fun invoke(input: Double): Double {
-        if (input == 1.0) return 1.0
-        if (input == -1.0) return -1.0
-
-        val sign = if (input == 0.0) 1.0 else input.sign
-
-        return sign * (1 / (1 + E.pow(-a * abs(input) - b)))
-    }
+    override operator fun invoke(input: Double): Double =
+        when (input) {
+            0.0 -> 0.0
+            1.0 -> 1.0
+            -1.0 -> -1.0
+            else -> input.sign * (1 / (1 + E.pow(-a * abs(input) - b)))
+        }
 }
